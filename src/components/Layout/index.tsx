3e8b315f02127304da60ab76780f6d094bde3e2f
ITDdev20 - Chen Hao Jie <ITDDEV20.intonly@hld.com>
2025-10-23 11:34:16 +0800

    feat: 引入clsx合并类名库

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,19 @@
 import { Layout as AntdLayout, Flex } from "antd";
+import clsx from "clsx";
 import styles from "./layout.module.scss";
-const { Header, Content } = AntdLayout;
 
+const { Header, Content } = AntdLayout;
 
 interface LayoutProps {
 	children: React.ReactNode;
 }
 export default function Layout({ children }: LayoutProps) {
 	return (
-		<AntdLayout className={styles.layoutContainer}>
-			<Header className={styles.header}>
+		<AntdLayout className={clsx(styles.layoutContainer)}>
+			<Header className={clsx(styles.header)}>
 				<Flex align="center" justify="space-between">
-					<h1>Facility System</h1>
-					<div className="menu">123</div>
+					<h1 className={clsx("text-base")}>Facility System</h1>
+					<div className={clsx("menu")}>123</div>
 				</Flex>
 			</Header>
 			<Content>{children}</Content>
